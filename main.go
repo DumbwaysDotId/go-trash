91cebfed4d69b3136e078bf2123a6b4ede16cfe4
radiegtya <radiegtya@yahoo.co.id>
2018-10-24 16:07:13 +0700

    6.ifelse

--- main.go
+++ main.go
@@ -5,17 +5,12 @@ import (
 )
 
 func main() {
-	// //fix length array
-	// var arr [3]int
-	// arr[2] = 2
-	// fmt.Println(arr)
-
-	// //short hand fix array
-	// arr := [3]int{1, 2, 3}
-	// fmt.Println(arr)
-
-	//short hand dynamic array
-	arr := []int{1, 2, 3}
-	arr = append(arr, 4)
-	fmt.Println(arr)
+	x := 2
+	if x < 3 {
+		fmt.Println("Ayee")
+	} else if x == 4 {
+		fmt.Println("is 4")
+	} else {
+		fmt.Println("Oops")
+	}
 }
