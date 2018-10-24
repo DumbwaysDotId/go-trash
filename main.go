c2493434e6f6319aa84ec3b19ba9ca4315e6da17
radiegtya <radiegtya@yahoo.co.id>
2018-10-24 15:53:41 +0700

    4.variable

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var x int = 5
+	var y int = 7
+	var sum int = x + y
+	var name string = "Ega"
+	var isLoading bool = false
+	var myFloatNumber float64 = 2.1
+
+	z := 10
+	address := "south tangerang"
+
+	fmt.Println(sum)
+	fmt.Println(name)
+	fmt.Println(isLoading)
+	fmt.Println(myFloatNumber)
+	fmt.Println(z)
+	fmt.Println(address)
 }
