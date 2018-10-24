78516665290bafdd548e0cc032f4751567004a80
radiegtya <radiegtya@yahoo.co.id>
2018-10-24 16:48:56 +0700

    11.pointer

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	Name string
-	Age  int
-}
+import (
+	"fmt"
+)
 
 func main() {
-	// p := Person{Name: "Ega", Age: 28}
+	i := 10
+	inc(&i)
+	fmt.Println(i)
+}
 
-	// fmt.Println(p)
-
-	// fmt.Println(p.Name)
-
-	//array of struct
-	p := []Person{
-		{Name: "Ega", Age: 28},
-		{Name: "Yuni", Age: 21},
-	}
-
-	// fmt.Println(p)
-	// fmt.Println(p[1])
-	// fmt.Println(p[1].Age)
-
-	//traverse over array of struct
-	for _, elem := range p {
-		fmt.Println("Name", elem.Name)
-		fmt.Println("Age", elem.Age)
-		fmt.Println("")
-	}
+func inc(x *int) {
+	*x++
 }
